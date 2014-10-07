bc0e4bfe3c7fb6e5b87cb9a61bb6423e6fc8a7f4
root <root@platform.vixyvideo.com>
2014-10-07 17:57:58 +0200

    Added restorescripts

--- testing(DANGER)/import-schema.sh
+++ testing(DANGER)/import-schema.sh
@@ -2,12 +2,26 @@
 
 . bin/setup.sh
 
-#prompt for backup folder
-echo "Enter the full system path to your backup folder"
-read  SQL_FOLDER
+#prompt for backup folder if it is not set in config
+if [ ! -d "$SQL_FOLDER" ]; then
+	echo "Enter the full system path to your backup folder"  
+	read  SQL_FOLDER
+fi
 
 if [ ! -d $SQL_FOLDER ]; then
 	echo "Invalid folder"
 	exit 1
 fi
 
+for FILE in `find $SQL_FOLDER -name '*.mysql'`
+do
+  echo "Importing $FILE"
+  FILENAME=$(basename $FILE)
+  SCHEMA=`echo $FILENAME | grep --color -P -o '[a-z_]*' | head -1`
+
+  echo "Creating schema $SCHEMA"
+  $(mysql -u$MYSQL_ROOT -p$MYSQL_ROOT_PW -e "CREATE DATABASE $SCHEMA;")
+
+  echo "Importing $FILE into $SCHEMA"
+  $(mysql -u$MYSQL_ROOT -p$MYSQL_ROOT_PW $SCHEMA < $FILE)
+done
--- testing(DANGER)/restore-schema.sh
+++ testing(DANGER)/restore-schema.sh
@@ -1,7 +1,7 @@
 #/usr/bin/env sh
 
 echo "- Drop schema"
-. testing(DANGER)/drop-schema.sh
+. testing\(DANGER\)/drop-schema.sh
 
 echo "- Restore schema"
-. testing(DANGER)/import-schema.sh
+. testing\(DANGER\)/import-schema.sh
