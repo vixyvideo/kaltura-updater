a55a566fba2e64c5c83557676816b5507037a69a
Joost Pluijmers <jpluijmers@digitalexpression.nl>
2014-10-13 13:13:41 +0200

    Log output to logfile

--- bin/run-update.sh
+++ bin/run-update.sh
@@ -4,8 +4,10 @@ if [ "$SETUP" != "1" ]; then
 	. bin/setup.sh
 fi
 
+TIMESTAMP=$(date +%s)
+
 for RELEASE in $RELEASES; do
  echo "--- Running update.php for $RELEASE"
  cd "$EXPORT_PATH/$RELEASE/deployment/updates"
- php "update.php"
+ php "update.php" > "$LOG_PATH/run-update-$TIMESTAMP" 2>&1
 done;
