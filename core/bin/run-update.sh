7273eb55e33efb186e0142462aa0527fe132eb97
root <root@platform.vixyvideo.com>
2014-11-05 19:12:04 +0100

    Log errors to a different file

--- core/bin/run-update.sh
+++ core/bin/run-update.sh
@@ -7,5 +7,5 @@ fi
 for RELEASE in $RELEASES; do
  echo "--- Running update.php for $RELEASE"
  cd "$EXPORT_PATH/$RELEASE/deployment/updates"
- php "update.php" >> "$LOG_FOLDER/update.log" 2>&1
+ php "update.php" >> "$LOG_FOLDER/update.log" 2> "$LOG_FOLDER/update.error.log"
 done;
